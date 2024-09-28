238e20ae47a21115d33650266602318ad56e9944
Changpeng Wang <mc3304158030@163.com>
2024-09-28 15:40:49 +0800

    11

--- hello.c
+++ hello.c
@@ -2,4 +2,11 @@
 int main() {
     printf("答辩\n");
     return 0;
-}8+
+}
+
+// 访问链接加入我的团队「上海电子信息职业技术学院」，一起体验 CODING 一站式软件研发管理协作平台。 https://e.coding.net/invite?key=65938369969232197db0 注册验证码为 261571
+
+// https://g-fkom6135.coding.net/p/zhenyun/d/car-rental--client/git
+
+// 这是商家端的仓库地址
+https://g-fkom6135.coding.net/p/zhenyun/d/car-rental--merchant/git
